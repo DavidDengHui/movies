c1d1f13f31290bfb446dd3a5853fe088a31fe347
DavidDengHui <david.no.1@msn.cn>
2018-01-20 21:17:15 +0800

    掩耳影院初级备份

---
+++ footer.php
@@ -0,0 +1,32 @@
+<div class="foornav">
+	<a href="/"><span><img src="/images/index.png"/>首页</span></a>
+	<a href="./movie.php?m=http://www.360kan.com/dianying/list.php?cat=all%26pageno=1"><span><img src="/images/video.png"/>电影</span></a>
+	<a href="./tv.php?u=http://www.360kan.com/dianshi/list.php?cat=all%26pageno=1"><span><img src="/images/dianshi.png"/>电视剧</span></a>
+	<a href="./dongman.php?m=http://www.360kan.com/dongman/list.php?cat=all%26pageno=1"><span><img src="/images/dianshiju.png"/>动漫</span></a>
+	<?php echo $aik['end_ad'];?>
+</div>
+<footer class="footer">
+<div class="branding branding-black">
+	<div class="container" style="text-align: center;">
+		<h2><?php echo $aik['sitename'];?> - 海量高清VIP视频免费观看</h2>
+						<a target="_blank" title="CovEar" class="gobtn" href="//covear.top">作者博客</a>	
+			</div>
+</div>
+<p style="padding: 0 4px;"><?php echo $aik['foot'];?><br/>管理员邮箱：<a href="mailto:<?php echo $aik['admin_email'];?>"><?php echo $aik['admin_email'];?></a><br /> <a target="_blank" href="http://www.miitbeian.gov.cn"><?php echo $aik['icp'];?></a><br/>Copyright &copy; 2017 <a href="<?php echo $aik['pcdomain'];?>"><?php echo $aik['sitename'];?></a>
+</footer>
+	<div class="rewards-popover-mask" etap="rewards-close"></div>
+	<div class="rewards-popover">
+		<h3>觉得本站还不错就打赏一下吧！</h3>
+				<div class="rewards-popover-item">
+			<h4>支付宝扫一扫打赏</h4>
+			<?php echo $aik['zfb_ad'];?>
+		</div>
+						<div class="rewards-popover-item">
+			<h4>微信扫一扫打赏</h4>
+			<?php echo $aik['wx_ad'];?>
+		</div>
+				<span class="rewards-popover-close" etap="rewards-close"></span>
+	</div> 
+<script type='text/javascript' src='js/main.js'></script>
+<script type="text/javascript"> document.body.oncontextmenu=document.body.ondragstart= document.body.onselectstart=document.body.onbeforecopy=function(){return false;};
+document.body.onselect=document.body.oncopy=document.body.onmouseup=function(){document.selection.empty();}; </script>
